b391c588d2f51f138cc09f2b7cd45e9920a27d64
Oliver Lloyd <ollielloyd96@gmail.com>
2020-08-30 11:59:10 +0100

    Custom palettes for plots

--- ollie-dir/fruitveg_foreign_bodies_plot.py
+++ ollie-dir/fruitveg_foreign_bodies_plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
 food_haz = pd.read_csv('data/tidy/food_hazards_foreign_bodies.csv')
@@ -38,12 +39,28 @@ for haz, df in counts.groupby('contaminant'):
     diff_df.loc[len(diff_df)] = row
 diff_df.sort_values('freq_diff', ascending=False, inplace=True)
 
+
+def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
+    c1=np.array(mpl.colors.to_rgb(c1))
+    c2=np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
+
+kepler_arc_blue = '#3748a6'
+kepler_arc_red ='#951416'
+header_blue = '#0E33FF'
+header_red = '#FD250B'
+color_vals = [(val-min(diff_df.freq_diff))/(max(diff_df.freq_diff)-min(diff_df.freq_diff)) for val in diff_df.freq_diff]
+color_vals_uniform = [x/len(color_vals) for x in range(len(color_vals), 0, -1)]
+color_vals2 = [np.mean(x) for x in zip(color_vals, color_vals_uniform)]
+colorlist = [colorFader(header_blue, header_red, val) for val in color_vals2]
+custom_palette = sns.color_palette(colorlist)
+
 plt.style.use("dark_background")
 plt.figure(figsize=(10, 8))
-sns.barplot('freq_diff', 'contaminant', data=diff_df, palette='RdBu')
+sns.barplot('freq_diff', 'contaminant', data=diff_df, palette=custom_palette)
 plt.ylabel('')
 plt.xlabel('Relative frequency change after switching to non-EU imports')
 plt.title('Foreign bodies in fruits and vegetables: EU vs non-EU imports')
 plt.tight_layout()
-plt.savefig('figures/foreign_bodies_eu_noneu.png')
+plt.savefig('foreign_bodies_eu_noneu.png')
 
